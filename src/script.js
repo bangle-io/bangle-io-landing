6199a51dfb47fb2a56a9f2c1659a61829f328896
Kushan Joshi <0o3ko0@gmail.com>
2021-10-14 20:30:22 -0400

    accordion

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import './scripts/accordion.js';
+
 class BangleLogo extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
@@ -66,6 +68,55 @@ class BangleLogo extends HTMLElement {
 if (!window.customElements.get('bangle-logo')) {
   window.BangleLogo = BangleLogo;
   window.customElements.define('bangle-logo', BangleLogo);
+}
+
+class ChevronDownIcon extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    class="chevron-down"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M6.34317 7.75732L4.92896 9.17154L12 16.2426L19.0711 9.17157L17.6569 7.75735L12 13.4142L6.34317 7.75732Z"
+      fill="currentColor"
+    />
+  </svg>`;
+    const className = this.getAttribute('child-class');
+    this.firstElementChild.setAttribute('class', className);
+  }
+}
+
+if (!window.customElements.get('chevron-down-icon')) {
+  window.ChevronDownIcon = ChevronDownIcon;
+  window.customElements.define('chevron-down-icon', ChevronDownIcon);
+}
+
+class ChevronUpIcon extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M17.6569 16.2427L19.0711 14.8285L12.0001 7.75739L4.92896 14.8285L6.34317 16.2427L12.0001 10.5858L17.6569 16.2427Z"
+      fill="currentColor"
+    />
+  </svg>`;
+    const className = this.getAttribute('child-class');
+    this.firstElementChild.setAttribute('class', className);
+  }
+}
+
+if (!window.customElements.get('chevron-up-icon')) {
+  window.ChevronUpIcon = ChevronUpIcon;
+  window.customElements.define('chevron-up-icon', ChevronUpIcon);
 }
 
 class BangleHeader extends HTMLElement {
---
+++ src/scripts/accordion.js
@@ -0,0 +1,88 @@
+// only works with h3
+class AccordionContainerElement extends HTMLElement {
+  constructor() {
+    super();
+    this._handleClick = this._handleClick.bind(this);
+    this.keys = {
+      down: 40,
+      up: 38,
+      home: 36,
+      end: 35,
+    };
+    this.accordionToggles = ['h3 > div[aria-expanded]'].join(',');
+  }
+
+  upgradeHeadings() {
+    const headings = Array.from(this.querySelectorAll('[data-summary]'));
+    headings.forEach((heading) => {
+      const parent = document.createElement('div');
+      parent.setAttribute('aria-expanded', 'false');
+      const children = Array.from(heading.childNodes);
+      children.forEach((child) => {
+        parent.appendChild(child.cloneNode(true));
+        heading.removeChild(child);
+      });
+      parent.appendChild(document.createElement('chevron-up-icon'));
+      heading.appendChild(parent);
+    });
+  }
+
+  upgradePanels() {
+    const panels = Array.from(this.querySelectorAll('[data-panel]'));
+    panels.forEach((panel) => panel.setAttribute('hidden', ''));
+  }
+
+  connectedCallback() {
+    this.upgradeHeadings();
+    this.upgradePanels();
+    this.addEventListener('click', this._handleClick, false);
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener('click', this._handleClick, false);
+  }
+
+  _handleClick(event) {
+    const toggle = event.target.closest(this.accordionToggles);
+    if (!toggle) return;
+
+    const panel = toggle.parentElement.nextElementSibling;
+    const eventShouldFire = this.dispatchEvent(
+      new CustomEvent('accordion-container-toggled', {
+        bubbles: true,
+        cancelable: true,
+        detail: {
+          toggle,
+          panel,
+        },
+      }),
+    );
+    if (!eventShouldFire) return;
+
+    const isExpanded =
+      toggle.getAttribute('aria-expanded') == 'true' ? true : false;
+
+    if (!isExpanded) {
+      toggle.parentElement.classList.add('expanded');
+      toggle.removeChild(toggle.querySelector('chevron-up-icon'));
+      toggle.appendChild(document.createElement('chevron-down-icon'));
+    } else {
+      toggle.parentElement.classList.remove('expanded');
+      toggle.removeChild(toggle.querySelector('chevron-down-icon'));
+      toggle.appendChild(document.createElement('chevron-up-icon'));
+    }
+
+    toggle.setAttribute('aria-expanded', !isExpanded);
+    isExpanded
+      ? panel.setAttribute('hidden', '')
+      : panel.removeAttribute('hidden');
+  }
+}
+
+if (!window.customElements.get('accordion-container')) {
+  window.AccordionContainerElement = AccordionContainerElement;
+  window.customElements.define(
+    'accordion-container',
+    AccordionContainerElement,
+  );
+}
